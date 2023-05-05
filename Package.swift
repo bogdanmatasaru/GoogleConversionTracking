ec14d3971b97699b16c3921eba705f70ca119fbb
bogdanmatasaru <bogdan.matasaru@yahoo.com>
2023-05-05 17:44:55 +0300

    updated package

--- Package.swift
+++ Package.swift
@@ -22,7 +22,13 @@ let package = Package(
             dependencies: [
                 .target(name: "GoogleConversionTracking")
             ],
-            publicHeadersPath:"include"
+            publicHeadersPath:"include",
+            cSettings: [
+               .unsafeFlags(["-w"])
+            ],
+            swiftSettings: [
+                            .unsafeFlags(["-suppress-warnings"]),
+                        ]
         ),
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
